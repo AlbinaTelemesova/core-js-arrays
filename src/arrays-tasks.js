1480ef23c27cee1c652fc3c5b0c2e17801027869
AlbinaT <telemesova96@gmail.com>
2025-10-13 01:07:05 +0500

    feat: add solution for getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -124,8 +124,16 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length > 0) {
+    const sum = arr.reduce((acc, cur) => acc + cur, 0);
+    const lengthOfArray = arr.length;
+    const averageNum = Math.round((sum / lengthOfArray) * 100) / 100;
+    return averageNum.isInteger
+      ? averageNum
+      : parseFloat(averageNum.toFixed(2));
+  }
+  return 0;
 }
 
 /**
